b4f3d9e8a47740e7536bf3c60c5088ce313dcfc4
diegolink77 <38171175+diegolink77@users.noreply.github.com>
2018-04-13 17:50:22 +0200

    Add files via upload

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC3GpCaX5BX1h1C8L5mpbgBfcidwT88eCc",
+  authDomain: "agendaprofesor-a8988.firebaseapp.com",
+  databaseURL: "https://agendaprofesor-a8988.firebaseio.com",
+  projectId: "agendaprofesor-a8988",
+  storageBucket: "agendaprofesor-a8988.appspot.com",
+  messagingSenderId: "640588255756"
+  };
